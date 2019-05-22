f8086c61278e4927e7e356f68e78aff203feb308
J_theGit <jevgenij.turonok@gmail.com>
2019-05-22 17:32:05 +0100

    code for adding to collection

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ const courseSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     isPublished: Boolean    
 });
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(author) {
+    const course = new Course({
+        name: 'node.js',
+        author: author,
+        tags: [
+            'one',
+            'two', 
+        ],
+        isPublished: false
+    });
+    return await course.save();
+}
+
+// createCourse('Jevgenij').then((res) => debug('created', res._id));
+// createCourse('Karolina').then((res) => debug('created', res._id));
+
