65e94d9decc94bdc789838c478a053f0658eb13c
J_theGit <jevgenij.turonok@gmail.com>
2019-05-22 16:31:30 +0100

    added schema

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ mongoose.connect(url, { useNewUrlParser: true })
     .then(() => debug('connected to', url))
     .catch(e => debug('could not connect', e.message));
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean    
+});
